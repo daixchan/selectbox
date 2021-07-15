5e21666a066382aa38654dc6028fd508663b2dfb
daixchan <daixchan@gmail.com>
2021-07-15 15:43:37 +0900

    누락된 파일 커밋

---
+++ src/SelectBox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SelectBox() {
+  return (
+    <select id="selectBox">
+      <option value="0">더기프팅컴퍼니</option>
+      <option value="1">월간가슴</option>
+      <option value="2">인더웨어</option>
+      <option value="3">프론트엔드 엔지니어</option>
+    </select>
+  );
+}
